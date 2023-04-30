57d06a1d2e2b4927673bea6342050803632557de
OrhanBC <67674376+OrhanBC@users.noreply.github.com>
2023-04-30 01:20:56 -0400

    Update instructor.py

--- instructor.py
+++ instructor.py
@@ -1,17 +1,23 @@
 import openai
 
 def instructions(output):
-    openai.api_key = "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"
+    openai.api_key = "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"
     
     # Set up the OpenAI API
-    model_engine = "text-davinci-002"
+    model_engine = "gpt-3.5-turbo"
 
-    prompt = f"Give me a walkthrough on how to style these items: {output}"
+    prompt = f"Give me a walkthrough on how to stylishly wear these clothing items: {output}"
     
-    response = openai.Completion.create(        
-        engine=model_engine,
-        prompt=prompt,
-        max_tokens=100
-        )
+    chatgpt_response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=[{"role": "user", "content": prompt}],
+            temperature=0.1,
+            max_tokens=2000,
+            top_p=0.95)
 
-    return response.choices[0].text
+    response = chatgpt_response['choices'][0]['message']['content'].strip()
+
+    index = response.index('1.')
+    response = response[index:]
+    print(response)
+    return response
